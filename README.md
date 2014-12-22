545a4f4ed0213ac8abc6dbf7311a4892f6f47236
Yosef <yosef@yosef.net>
2014-12-22 16:34:19 -0600

    Added support for http paths via package_src_http parameter and made module a bit cleaner

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ puppet-nxlog
 
 Puppet installer for nxLog on Windows.
 Should be a generic module meant to be customized via hiera.
+Supports unc path for source, as well as http(s)/ftp/puppet/s3
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,8 @@ class nxlog (
     $package_name               =   "NXLOG-CE",
     $package_version            =   "2.8.1248",
     $install_dir                =   "C:\\Program Files (x86)\\nxlog\\",
-    $package_src                =   "\\\\path\\to\\nxlog-ce-2.8.1248.msi",
+    $package_src                =   "\\path\\to\\nxlog-ce-2.8.1248.msi", # unc
+    $package_src_http           =   undef, # if defined, HTTP, ftp, local, s3
     $service_name               =   "nxlog",
     $service_ensure             =   running,
     $config_dir                 =   "C:\\Program Files (x86)\\nxlog\\conf\\",
@@ -18,46 +19,64 @@ class nxlog (
     $nxlog_logfile              =   "C:\\Program Files (x86)\\nxlog\\data\\nxlog.log",
 )
 {
+  require staging
+
+  
+
     $local_package_msi = "${temp_media_dir}${package_name}-${package_version}.msi"
 
     if "${operatingsystem}" == 'windows' {
 
         if $package_ensure == 'installed' or $package_ensure == 'present' {
+
+            
             service { $service_name:
                 ensure  => $service_ensure,
                 require => Package[$package_name],
-            }        
-        }
+            }
 
-        file { "${temp_media_dir}" :
-            ensure => 'directory',
-            recurse => true,
-        } ->
+            package { $package_name:
+                ensure  => $package_ensure,
+                source  => $local_package_msi,
+            }  
 
-        file { "${local_package_msi}" :
-            ensure => 'file',
-            source => "${package_src}",
-        }
+            if ! defined(File[$temp_media_dir]) {
+                file { $temp_media_dir:
+                    ensure=>directory,
+                }
+            }      
 
-  
-            
-        package { $package_name:
-            ensure  => $package_ensure,
-            source  => $local_package_msi,
-        }
-        
-        file { "${config_dir}${config_file}":
-            ensure  => present,
-            content => regsubst(template('nxlog/nxlog.conf.erb'), '\n', "\r\n", 'EMG'),
-            notify  => Service[$service_name],
-            require => Package[$package_name],
-        }   
-    }
+            if $package_src_http == undef {
+               file { "${local_package_msi}" :
+                    ensure => 'file',
+                    source => "${package_src}",
+                    before => Package["${package_name}"],
+                    replace => false,
+                    source_permissions => ignore,
+               }
+            }
+            else{
+                notice("Starting the staging execution")
+                staging::file { "${package_name}-${package_version}.msi":
+                    source  => "${package_src_http}",
+                    target  => "${local_package_msi}",
+                    before => Package["${package_name}"],
+               }
+            }
+    
+            file { "${config_dir}${config_file}":
+                ensure  => present,
+                content => regsubst(template('nxlog/nxlog.conf.erb'), '\n', "\r\n", 'EMG'),
+                notify  => Service[$service_name],
+                require => Package[$package_name],
+            } 
+
+
+        } 
+    }      
     else{
-        notify { 'The module is only supported on windows - no action taken.':  
-        }
-    }
+        notice("NXLOG: Not supported on non-windows platforms at this time.")
+    } 
+      
 
-
-    #create_resources(nxlog::conf,hiera('nxlog::conf::inputs'))
 }
