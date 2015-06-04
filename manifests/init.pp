4149fa85872a86a7c48c5d2ee818700def3a5538
Choi, Sunghee <Sunghee.Choi@cdk.com>
2015-06-04 16:14:51 -0500

    modified temp folder structure a bit

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,11 +9,11 @@ class nxlog (
     $service_ensure             =   running,
     $config_dir                 =   "C:\\Program Files (x86)\\nxlog\\conf\\",
     $config_file                =   "nxlog.conf",
-    $temp_media_dir             =   "C:\\Media\\",
+    $temp_media_dir             =   'C:\media',
     $include_external_configs   =   false,
     $external_config_path       =   "C:\\nxlog\\configuration\\*.nxlog.conf",
     $nxlog_moduledir            =   "C:\\Program Files (x86)\\nxlog\\modules",
-    $nxlog_cachedir             =   "C:\\Program Files (x86)\\nxlog\\data",                
+    $nxlog_cachedir             =   "C:\\Program Files (x86)\\nxlog\\data",
     $nxlog_pidfile              =   "C:\\Program Files (x86)\\nxlog\\data\\nxlog.pid",
     $nxlog_spooldir             =   "C:\\Program Files (x86)\\nxlog\\data",
     $nxlog_logfile              =   "C:\\Program Files (x86)\\nxlog\\data\\nxlog.log",
@@ -21,8 +21,9 @@ class nxlog (
 {
   include staging
 
+  $src_dir = "${temp_media_dir}\\puppet_nxlog"
 
-    $local_package_msi = "${temp_media_dir}${package_name}-${package_version}.msi"
+  $local_package_msi = "${src_dir}\\${package_name}-${package_version}.msi"
 
     if "${::operatingsystem}" == 'windows' {
 
@@ -32,40 +33,41 @@ class nxlog (
 
         if $package_ensure == 'installed' or $package_ensure == 'present' {
 
-            
-            
+
+
             package { $package_name:
                 ensure  => $package_ensure,
                 source  => $local_package_msi,
                 require => File["${local_package_msi}"],
             }
-            
+
             file { "${config_dir}${config_file}":
                 ensure  => present,
                 content => regsubst(template('nxlog/nxlog.conf.erb'), '\n', "\r\n", 'EMG'),
                 require => Package["${package_name}"],
-            } 
-            
+            }
+
             service { $service_name:
                 ensure  => $service_ensure,
                 require => File["${config_dir}${config_file}"],
             }
-            
-            if ! defined(File[$temp_media_dir]) {
-                file { $temp_media_dir:
+
+            if ! defined(File["${temp_media_dir}"]) {
+                file { "${temp_media_dir}":
                     ensure=>directory,
                 }
-            }     
-
-
-
-
+            }
+          if ! defined(File["${src_dir}"]){
+            file { "${src_dir}":
+              ensure => directory,
+            }
+          }
             if $package_src_http == undef {
                file { "${local_package_msi}" :
                     ensure => 'file',
                     source => "${package_src}",
                     replace => false,
-                    require => File[$temp_media_dir],
+                    require => File[$src_dir],
                }
             }
             else{
@@ -77,18 +79,18 @@ class nxlog (
                   ensure => 'file',
                   source => "${staging::path}/nxlog/${package_name}-${package_version}.msi",
                   replace => false,
-                  require => File[$temp_media_dir],
+                  require => File[$src_dir],
                 }
             }
 
-            
 
 
-        } # if $package_ensure == 'installed' or $package_ensure == 'present' 
+
+        } # if $package_ensure == 'installed' or $package_ensure == 'present'
     }# if "${::operatingsystem}" == 'windows'
     else{
         notice("NXLOG: Not supported on non-windows platforms at this time.")
-    }# else of (if "${::operatingsystem}" == 'windows') 
-      
+    }# else of (if "${::operatingsystem}" == 'windows')
+
 
 }
